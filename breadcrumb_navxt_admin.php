77883dfb3aa350ea2db68f6221538b944de7089f
mtekk <mtekk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-02 01:59:43 +0000

    Fixing internal numbering to be 2.1.3
    git-svn-id: https://plugins.svn.wordpress.org/breadcrumb-navxt/tags/2.1.3@52738 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- breadcrumb_navxt_admin.php
+++ breadcrumb_navxt_admin.php
@@ -3,7 +3,7 @@
 Plugin Name: Breadcrumb NavXT - Adminstration Interface
 Plugin URI: http://mtekk.weblogs.us/code/breadcrumb-navxt/
 Description: Adds a breadcrumb navigation showing the visitor&#39;s path to their current location. This enables the administrative interface for specifying the output of the breadcrumb. For details on how to use this plugin visit <a href="http://mtekk.weblogs.us/code/breadcrumb-navxt/">Breadcrumb NavXT</a>. 
-Version: 2.1.2
+Version: 2.1.3
 Author: John Havlik
 Author URI: http://mtekk.weblogs.us/
 */
