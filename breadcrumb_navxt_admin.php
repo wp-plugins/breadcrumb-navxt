a8ffb487d2a55467b92aaca76f6068a34e176950
mtekk <mtekk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-29 15:20:27 +0000

    Merged 3.0.2 advances into the trunk.
    git-svn-id: https://plugins.svn.wordpress.org/breadcrumb-navxt/trunk@82851 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- breadcrumb_navxt_admin.php
+++ breadcrumb_navxt_admin.php
@@ -158,24 +158,31 @@ class bcn_admin
 				$this->delete_option('bcn_singleblogpost_tag_prefix');
 				$this->delete_option('bcn_singleblogpost_tag_suffix');
 			}
-			//Check if we have valid anchors
-			if($temp = $this->get_option('bcn_options'))
-			{
-				if(strlen($temp['home_anchor']) == 0 || 
-					strlen($temp['blog_anchor']) == 0 || 
-					strlen($temp['page_anchor']) == 0 || 
-					strlen($temp['post_anchor']) == 0 || 
-					strlen($temp['tag_anchor']) == 0 ||
-					strlen($temp['date_anchor']) == 0 ||
-					strlen($temp['category_anchor']) == 0)
-				{
-					$this->delete_option('bcn_options');
-				}
-			}
 			//Always have to update the version
 			$this->update_option('bcn_version', $this->version);
 			//Store the options
 			$this->add_option('bcn_options', $this->breadcrumb_trail->opt);
+		}
+		//Check if we have valid anchors
+		if($temp = $this->get_option('bcn_options'))
+		{
+			//Missing the blog anchor is a bug from 3.0.0/3.0.1 so we soft error that one
+			if(strlen($temp['blog_anchor']) == 0)
+			{
+				$temp['blog_anchor'] = $this->breadcrumb_trail->opt['blog_anchor'];
+				$this->update_option('bcn_options', $temp);
+			}
+			else if(strlen($temp['home_anchor']) == 0 || 
+				strlen($temp['blog_anchor']) == 0 || 
+				strlen($temp['page_anchor']) == 0 || 
+				strlen($temp['post_anchor']) == 0 || 
+				strlen($temp['tag_anchor']) == 0 ||
+				strlen($temp['date_anchor']) == 0 ||
+				strlen($temp['category_anchor']) == 0)
+			{
+				$this->delete_option('bcn_options');
+				$this->add_option('bcn_options', $this->breadcrumb_trail->opt);
+			}
 		}
 	}
 	/**
@@ -896,15 +903,7 @@ class bcn_admin
 	 */
 	function add_option($key, $value)
 	{
-		//If in a WPMU environment
-		if(function_exists('get_current_site'))
-		{
-			return add_blog_option(get_current_site()->id, $key, $value);
-		}
-		else
-		{
-			return add_option($key, $value);
-		}
+		return add_option($key, $value);
 	}
 	/**
 	 * delete_option
@@ -916,15 +915,7 @@ class bcn_admin
 	 */
 	function delete_option($key)
 	{
-		//If in a WPMU environment
-		if(function_exists('get_current_site'))
-		{
-			return delete_blog_option(get_current_site()->id, $key);
-		}
-		else
-		{
-			return delete_option($key);
-		}
+		return delete_option($key);
 	}
 	/**
 	 * update_option
@@ -937,15 +928,7 @@ class bcn_admin
 	 */
 	function update_option($key, $value)
 	{
-		//If in a WPMU environment
-		if(function_exists('get_current_site'))
-		{
-			return update_blog_option(get_current_site()->id, $key, $value);
-		}
-		else
-		{
-			return update_option($key, $value);
-		}
+		return update_option($key, $value);
 	}
 	/**
 	 * get_option
@@ -959,15 +942,7 @@ class bcn_admin
 	 */
 	function get_option($key, $safe = false)
 	{
-		//If in a WPMU environment
-		if(function_exists('get_current_site'))
-		{
-			$db_data = get_blog_option(get_current_site()->id, $key);
-		}
-		else
-		{
-			$db_data = get_option($key);
-		}
+		$db_data = get_option($key);
 		if($safe)
 		{
 			//If we get an array, we should loop through all of its members
