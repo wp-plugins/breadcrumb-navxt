ad4519f80dcd51f86f73cffc32f9d865d72a4850
mtekk <mtekk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-22 03:39:18 +0000

    Adding preliminary WP 2.8 widget class
    git-svn-id: https://plugins.svn.wordpress.org/breadcrumb-navxt/trunk@186020 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ breadcrumb_navxt_widget.php
@@ -0,0 +1,80 @@
+<?php
+/*  
+	Copyright 2007-2009  John Havlik  (email : mtekkmonkey@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+class bcn_widget extends WP_Widget
+{
+	//Default constructor
+	function __construct()
+	{
+		$ops = array('classname' => 'widget_breadcrumb_navxt', __('This widget adds a breadcrumb trail to your widget sidebar.'));
+		//We're going to be a bit evil here and do things the PHP5 way
+		parent::__construct('bcn_widget', 'Breadcrumb NavXT', $ops);
+	}
+	function widget($args, $instance)
+	{
+		extract($args);
+		//Manditory before widget junk
+		echo $before_widget;
+		//We'll want to switch between the two breadcrumb output types
+		if($instance['list'] = true)
+		{
+			//Display the list output breadcrumb
+			echo '<ul class="breadcrumb_trail">';
+			bcn_display_list(true, $instance['linked'], $instance['reverse']);
+			echo '</ul>';
+		}
+		else
+		{
+			//Display the regular output breadcrumb
+			echo '<div class="breadcrumb_trail">';
+			bcn_display(true, $instance['linked'], $instance['reverse']);
+			echo '</div>';
+		}
+		//Manditory after widget junk
+		echo $after_widget;
+	}
+	function update($new_instance, $old_instance)
+	{
+		$instance = $old_instance;
+		//Filter out anything that could be invalid
+		$instance['list'] = (bool) esc_attr($new_instance['list']);
+		$instance['linked'] = (bool) esc_attr($new_instance['linked']);
+		$instance['reverse'] = (bool) esc_attr($new_instance['reverse']);
+		return $instance;
+	}
+	function form($instance)
+	{
+		//Filter out anything that could be invalid
+		$list = (bool) esc_attr($instance['list']);
+		$linked = (bool) esc_attr($instance['linked']);
+		$reverse = (bool) esc_attr($instance['reverse']);?>
+		<p>
+			<input class="widefat" type="checkbox" name="<?php $this->get_feild_name('list'); ?>" id="<?php $this->get_feild_id('list'); ?>" value="true" <?php checked(true, $list);?> />
+			<label for="<?php echo $this->get_field_id('list'); ?>"> <?php _e('Output trail as a list.'); ?></label>
+		</p>
+		<p>
+			<input class="widefat" type="checkbox" name="<?php $this->get_feild_name('linked'); ?>" id="<?php $this->get_feild_id('linked'); ?>" value="true" <?php checked(true, $linked);?> />
+			<label for="<?php echo $this->get_field_id('linked'); ?>"> <?php _e('Allow the breadcrumbs to be linked.'); ?></label>
+		</p>
+		<p>
+			<input class="widefat" type="checkbox" name="<?php $this->get_feild_name('reverse'); ?>" id="<?php $this->get_feild_id('reverse'); ?>" value="true" <?php checked(true, $reverse);?> />
+			<label for="<?php echo $this->get_field_id('reverse'); ?>"> <?php _e('Reverse the order of the trail.'); ?></label>
+		</p>
+		<?php
+	}
+}
